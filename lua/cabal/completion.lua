7000c7311db030d44e52f00a7a1fdd08e4b0313e
Nicholas Burley <nick.burley@protonmail.com>
2023-04-01 12:27:33 -0500

    Configured some LSP servers

--- lua/cabal/completion.lua
+++ lua/cabal/completion.lua
@@ -48,9 +48,22 @@ cmp.setup {
     }
 }
 
+-- Setup LSP servers
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require'lspconfig'.clangd.setup {
     capabilities = capabilities,
+}
+require'lspconfig'.cmake.setup{
+    capabilities = capabilities
+}
+require'lspconfig'.lua_ls.setup{
+    capabilities = capabilities
+}
+require'lspconfig'.jsonls.setup{
+    capabilities = capabilities
+}
+require'lspconfig'.pyright.setup{
+    capabilities = capabilities
 }
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
