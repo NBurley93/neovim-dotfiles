91fa959818b0d7fb5f2bc79ad87d035ef331b859
Nick Burley <nick.burley@protonmail.com>
2023-04-09 14:37:53 -0500

    Fixed python3 detection for windows

--- lua/cabal/options.lua
+++ lua/cabal/options.lua
@@ -1,34 +1,34 @@
 return {
-    config = function()
-        local options = {
-            fileformat = 'unix',
-            autoindent = true,
-            encoding = 'UTF-8',
-            updatetime = 250,
-            timeout = true,
-            timeoutlen = 300,
-            expandtab = true,
-            title = true,
-            shiftwidth = 4,
-            tabstop = 4,
-            backspace = 'indent,eol,start',
-            smartindent = true,
-            number = true,
-            relativenumber = false,
-            signcolumn = 'yes:1',
-            clipboard = 'unnamedplus',
-            termguicolors = true,
-            cursorline = true,
-            cot = 'menu,menuone,noselect',
-        }
+	config = function()
+		local options = {
+			fileformat = "unix",
+			autoindent = true,
+			encoding = "UTF-8",
+			updatetime = 250,
+			timeout = true,
+			timeoutlen = 300,
+			expandtab = true,
+			title = true,
+			shiftwidth = 4,
+			tabstop = 4,
+			backspace = "indent,eol,start",
+			smartindent = true,
+			number = true,
+			relativenumber = false,
+			signcolumn = "yes:1",
+			clipboard = "unnamedplus",
+			termguicolors = true,
+			cursorline = true,
+			cot = "menu,menuone,noselect",
+		}
 
-        for k, v in pairs(options) do
-            vim.opt[k] = v
-        end
+		for k, v in pairs(options) do
+			vim.opt[k] = v
+		end
 
-        -- Configure python runtime for windows
-        if (vim.fn.has('win32') or vim.fn.has('win32unix')) then
-            vim.g.python3_host_prog = '~\\.pyenv\\pyenv-win\\versions\\3.11.2\\python.exe'
-        end
-    end,
+		-- Configure python runtime for windows
+		if vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1 then
+			vim.g.python3_host_prog = "~\\.pyenv\\pyenv-win\\versions\\3.11.2\\python.exe"
+		end
+	end,
 }
