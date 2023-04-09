ec212655510733334e39d87e11206976b732a58f
Nick Burley <nick.burley@protonmail.com>
2023-04-09 13:46:35 -0500

    Updated mason-nvim-dap to 2.x

--- lua/cabal/dap.lua
+++ lua/cabal/dap.lua
@@ -1,42 +1,73 @@
 return {
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		local mason = require("mason-nvim-dap")
 
-    require('mason-nvim-dap').setup {
-      automatic_setup = true,
+		mason.setup({
+			automatic_setup = true,
 
-      ensure_installed = {
-        'python',
-        'cppdbg',
-      },
-    }
+			handlers = {
+				function(config)
+					mason.default_setup(config)
+				end,
+				python = function(config)
+					config.adapters = {
+						type = "executable",
+						command = "python",
+						args = {
+							"-m",
+							"-debugpy.adapter",
+						},
+					}
+					mason.default_setup(config)
+				end,
+			},
 
-    require('mason-nvim-dap').setup_handlers()
+			ensure_installed = {
+				"cpplint",
+				"clang_format",
+				"pylint",
+				"flake8",
+				"autopep8",
+				"isort",
+				"terraform_validate",
+				"terraform_fmt",
+				"stylua",
+				"cmake_lint",
+				"cmake_format",
+				"prettier",
+				"yamllint",
+				"markdownlint",
+				"hadolint",
+				"debugpy",
+				"cpptools",
+			},
+		})
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-        },
-      },
-    }
+		-- Dap UI setup
+		-- For more information, see |:help nvim-dap-ui|
+		dapui.setup({
+			-- Set icons to characters that are more likely to work in every terminal.
+			--    Feel free to remove or use ones that you like more! :)
+			--    Don't feel like these are good choices.
+			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+			controls = {
+				icons = {
+					pause = "⏸",
+					play = "▶",
+					step_into = "⏎",
+					step_over = "⏭",
+					step_out = "⏮",
+					step_back = "b",
+					run_last = "▶▶",
+					terminate = "⏹",
+				},
+			},
+		})
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-  end,
+		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+		dap.listeners.before.event_exited["dapui_config"] = dapui.close
+	end,
 }
