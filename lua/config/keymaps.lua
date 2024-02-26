51619b531efeccd9fd706587ca785daed987f787
Nicholas Burley <nick.burley@protonmail.com>
2024-02-26 00:06:29 -0600

    feat: Improved write/quit keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,20 @@
 local nvim_map = vim.keymap.set
+
 local nvim_cmd = vim.cmd
+
 local nvim_input = vim.fn.input
+
+local nvim_notify_warn = function(msg)
+	vim.notify(msg, vim.log.levels.WARN)
+end
+
+local nvim_notify_error = function(msg)
+	vim.notify(msg, vim.log.levels.ERROR)
+end
+
+local nvim_notify_info = function(msg)
+	vim.notify(msg, vim.log.levels.INFO)
+end
 
 -- Git
 local function git_mappings()
@@ -9,28 +23,37 @@ local function git_mappings()
 		nvim_cmd.Git("add " .. toStage)
 		print("Staged " .. toStage)
 	end, { desc = "Add currently open file to git staged" })
+
 	nvim_map("n", "<leader>gc", function()
 		nvim_cmd.Git("commit")
 	end, { desc = "Makes a git commit" })
+
 	nvim_map("n", "<leader>gs", nvim_cmd.Git, { desc = "Open up fugitive for git" })
+
 	nvim_map("n", "<leader>gp", function()
 		nvim_cmd.Git("-c push.default=current push")
 	end, { desc = "Push to git-remote" })
+
 	nvim_map("n", "<leader>gP", function()
 		nvim_cmd.Git("pull")
 	end, { desc = "Pull from git-remote" })
+
 	nvim_map("n", "<leader>gd", nvim_cmd.Gdiffsplit, { desc = "Open up Gdiff in vim-fugitive for the current file" })
+
 	nvim_map("n", "<leader>gr", function()
 		nvim_cmd.Git("diff --staged")
 	end, { desc = "Show a review diff for all our staged changes" })
+
 	nvim_map("n", "<leader>gb", function()
 		nvim_cmd.Git("branch ")
 	end, { desc = "Git branch" })
+
 	nvim_map("n", "<leader>go", function()
 		nvim_cmd.Git("checkout ")
 	end, { desc = "Git checkout" })
 
 	nvim_map("n", "<leader>dvo", nvim_cmd.DiffviewOpen, { desc = "Review diffs in diffview" })
+
 	nvim_map("n", "<leader>dvc", nvim_cmd.DiffviewClose, { desc = "Close diffview if open" })
 end
 
@@ -39,37 +62,48 @@ local function telescope_mappings()
 	local telescope = require("telescope")
 	local builtin = require("telescope.builtin")
 	local utils = require("telescope.utils")
+
 	nvim_map("n", "<leader>pf", builtin.find_files, { desc = "Find files in project" })
+
 	nvim_map("n", "<C-p>", function()
 		local _, ret, _ = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
 		if ret == 0 then
 			builtin.git_files({ show_untracked = true })
 		else
+			nvim_notify_info("Not a git repository, defaulting to find_files instead!")
 			builtin.find_files()
 		end
 	end, { desc = "Only search for files that are unignored by git" })
+
 	nvim_map("n", "<leader>ps", function()
 		builtin.grep_string({ search = nvim_input("Grep > ") })
 	end, { desc = "Perform a grep string search thru project" })
+
 	nvim_map("n", "<leader>vh", builtin.help_tags, { desc = "Search help tags" })
 	nvim_map("n", "<leader>se", function()
 		nvim_cmd.Telescope("emoji")
 	end, { desc = "Find emojis" })
+
 	nvim_map("n", "<leader>vat", function()
 		nvim_cmd.Telescope("terraform_doc", "full_name=hashicorp/aws")
 	end, { desc = "Search AWS Terraform Docs" })
+
 	nvim_map("n", "<leader>sm", function()
 		nvim_cmd.Telescope("http", "list")
 	end, { desc = "Telescope - Http Status Codes" })
+
 	nvim_map("n", "<leader>sgb", function()
 		builtin.git_branches({ show_remote_tracking_branches = false })
 	end, { desc = "Search git branches" })
+
 	nvim_map("n", "<leader>sgB", function()
 		builtin.git_branches({ show_remote_tracking_branches = true })
 	end, { desc = "Search git branches - Including upstream" })
+
 	nvim_map("n", "<leader>sb", function()
 		builtin.buffers({ sort_lastused = true, ignore_current_buffer = true })
 	end, { desc = "Show open buffers" })
+
 	nvim_map("n", "<leader>vn", function()
 		telescope.extensions.notify.notify()
 	end, { desc = "Show notify message history" })
@@ -101,6 +135,7 @@ local function neotest_mappings()
 	nvim_map("n", "<leader>ut", function()
 		require("neotest").run.run()
 	end, { desc = "Run the nearest unit-test" })
+
 	nvim_map("n", "<leader>uT", function()
 		require("neotest").run.run(vim.fn.expand("%"))
 	end, { desc = "Run all unit-tests in the active buffer's file" })
@@ -108,9 +143,19 @@ local function neotest_mappings()
 	nvim_map("n", "<leader>ud", function()
 		require("neotest").run.run({ strategy = "dap" })
 	end, { desc = "Run test with dap-ui debugging" })
+
 	nvim_map("n", "<leader>uD", function()
 		require("neotest").run.run({ strategy = "dap" })
 	end, { desc = "Run all tests in file with dap-ui debugging" })
+end
+
+local function attempt_write(write_func)
+	local is_modified = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(0), "readonly")
+	if is_modified == false then
+		write_func()
+	else
+		nvim_notify_warn("Cannot write read-only buffer!")
+	end
 end
 
 local function base_mappings()
@@ -126,6 +171,20 @@ local function base_mappings()
 	-- Scroll pages with the cursor on the center of the page
 	nvim_map("n", "<C-d>", "<C-d>zz")
 	nvim_map("n", "<C-u>", "<C-u>zz")
+
+	-- File write operations
+	nvim_map("n", "<leader>wb", function()
+		attempt_write(nvim_cmd.w)
+	end, { desc = "Write current buffer" })
+	nvim_map("n", "<leader>wq", function()
+		attempt_write(nvim_cmd.wq)
+	end, { desc = "Write current buffer and quit" })
+
+	-- Quit ops
+	nvim_map("n", "<leader>qq", nvim_cmd.qall, { desc = "Quit neovim, unless there are buffer changes" })
+	nvim_map("n", "<leader>qf", function()
+		nvim_cmd("qall!")
+	end, { desc = "Force-quit neovim even with buffer changes. VERY DANGEROUS" })
 
 	-- Reconfigure search next & prev to center the find result on the screen
 	nvim_map("n", "n", "nzzzv")
