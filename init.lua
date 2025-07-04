7260efc697b62a9c40db98967a77082787e0a342
Nicholas Burley <nick.burley@protonmail.com>
2025-07-04 18:22:01 -0500

    feat: further cleanup

--- init.lua
+++ init.lua
@@ -12,13 +12,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Bootstrap in external depencencies (tidy)
-if vim.fn.executable("tidy") == 0 then
-	-- Install tidy if on linux, otherwise, warn the user
-	if vim.fn.has("win32") == 0 then
-		os.execute("sudo apt-get install tidy -y")
-	end
-end
-
 -- Load configurations
 require("config").config_all()
