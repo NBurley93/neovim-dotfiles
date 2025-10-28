2f6fbb5779f1da3065414b6fd8f8035de57e9ab8
Nicholas Burley <nick.burley@protonmail.com>
2025-10-28 11:19:44 -0500

    feat : Improved neogit pane

--- lua/config/keymaps/git.lua
+++ lua/config/keymaps/git.lua
@@ -3,7 +3,7 @@ return {
     local neogit = require('neogit')
 
     vim.keymap.set('n', '<leader>gs', function()
-      neogit.open({ kind = 'floating' })
+      neogit.open({ kind = 'split_below_all' })
     end, { desc = 'View Git status' })
     vim.keymap.set('n', '<leader>dr', vim.cmd.DiffviewOpen, { desc = 'Review diffs in diffview' })
     vim.keymap.set('n', '<leader>dh', vim.cmd.DiffviewFileHistory, { desc = 'Opens the git file history view' })
