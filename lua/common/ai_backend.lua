5d160d46f3c3eb2d19777fbaa8210b451bc07abd
Nicholas Burley <nick.burley@protonmail.com>
2025-10-28 11:13:49 -0500

    feat : copilot chat theming enhancements

--- lua/common/ai_backend.lua
+++ lua/common/ai_backend.lua
@@ -4,17 +4,13 @@ M.baseline_config = {
   temperature = 0.5,
   language = 'English',
   window = {
-    layout = 'float',
-    width = 0.5,
-    height = 0.8,
-    border = 'rounded',
-    title = '🤖 Copilot Chat',
-    zindex = 100,
+    layout = 'vertical',
+    width = 0.33,
   },
   headers = {
-    user = '👤 User',
-    assistant = '🤖 Copilot',
-    tool = '🔧 Tool',
+    user = '🤔 User',
+    assistant = '🖥 AI Assistant',
+    tool = '🛠 Tooling',
   },
   prompts = {
     TaskDecomposer = {
@@ -22,12 +18,14 @@ M.baseline_config = {
       description = 'Get help breaking down projects into manageable tasks and subtasks.',
     },
   },
-  auto_insert_mode = true,
+  auto_insert_mode = false,
+  auto_fold = true,
+  separator = '══ஓ๑♡✧ ♡๑ஓ══',
 }
 
 function M.override_github_provider_url()
   -- Returns the default github provider, but changes the url to api.business.githubcopilot.com
-  local providers = require 'CopilotChat.config.providers'
+  local providers = require('CopilotChat.config.providers')
   local provider = providers.copilot
   provider.get_url = function()
     return 'https://api.business.githubcopilot.com/v1/chat/completions'
@@ -35,7 +33,7 @@ function M.override_github_provider_url()
 end
 
 function M.provision_config()
-  local chutes_provider = require 'common.ai_backends.chutes'
+  local chutes_provider = require('common.ai_backends.chutes')
   local cfg = M.baseline_config
 
   if chutes_provider.use_chutes() then
