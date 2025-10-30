a96fec4e466efa44c297a3fc73387fa2cd4eb3bf
Nicholas Burley <nick.burley@protonmail.com>
2025-10-30 09:03:38 -0500

    tweak : Telescope tweaks, update plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,32 +1,47 @@
-local icons = require 'common.icons'
+local icons = require('common.icons')
 
 return {
   {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' },
     config = function()
-      local ts = require 'telescope'
+      local ts = require('telescope')
 
-      ts.setup {
+      ts.setup({
         ['ui-select'] = {
-          require('telescope.themes').get_dropdown {},
+          require('telescope.themes').get_dropdown({}),
         },
         defaults = {
           layout_config = { prompt_position = 'top', height = 0.92, width = 0.92 },
           layout_strategy = 'flex',
           sorting_strategy = 'ascending',
-          winblend = 0,
+          -- winblend = 20,
           -- prompt_prefix = " ".. icons.ui.Telescope .. " ",
           -- selection_caret = "  ",
+          show_line = false,
           results_title = '',
-          dynamic_preview_title = true,
+          prompt_title = '',
+          preview_title = '',
+          dynamic_preview_title = false,
           border = true,
           color_devicons = true,
           -- Solid border
           -- borderchars = { "" },
           prompt_prefix = '  ', -- looks nice in neon; change if your font sulks
           selection_caret = ' ',
-          borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+          -- borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+          -- borderchars = { '═', '║', '═', '║', '╔', '╗', '╝', '╚' },
+          borderchars = {
+            prompt = { '━', '┃', '━', '┃', '┏', '┓', '┛', '┗' },
+            results = { '━', '┃', '━', '┃', '┏', '┓', '┛', '┗' },
+            preview = { '━', '┃', '━', '┃', '┏', '┓', '┛', '┗' },
+          },
+          -- borderchars = { '━', '┃', '━', '┃', '┏', '┓', '┛', '┗' },
+          -- borderchars = {
+          --   prompt = { '▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' },
+          --   results = { ' ', '▐', '▄', '▌', '▛', '▜', '▟', '▙' },
+          --   preview = { '▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' },
+          -- },
         },
         pickers = {
           find_files = { hidden = true },
@@ -45,9 +60,9 @@ return {
             },
           },
         },
-      }
-      ts.load_extension 'ui-select'
-      ts.load_extension 'git_branch'
+      })
+      ts.load_extension('ui-select')
+      ts.load_extension('git_branch')
     end,
   },
   { 'nvim-telescope/telescope-ui-select.nvim' },
